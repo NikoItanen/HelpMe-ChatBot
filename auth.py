96ed875b92333ba411dddad8572aa58b2d00a58f
Niko Itänen <120341039+NikoItanen@users.noreply.github.com>
2024-02-26 20:06:35 +0200

    The authentication implementation works now!
    Added sign up, sign in, and sign out implementation.

--- auth.py
+++ auth.py
@@ -5,7 +5,12 @@ auth_bp = Blueprint('Auth', __name__)
 
 @auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
-    
+    return render_template('login.html')
+
+
+@auth_bp.route('/loginHandle', methods=['GET', 'POST'])
+def loginHandle():
+
     #Create login route implementation here:
     if request.method == 'POST':
         username = request.form['username']
@@ -16,7 +21,8 @@ def login():
         user_data = user.get_user(username)
         
         if user_data and user.check_password(user_data, password):
-            session['user_id'] = user.data['username']
+            session['username'] = user_data['username']
+            print(session)
             flash('Login Successful!', 'success')
             return redirect(url_for('index'))
         else:
@@ -26,25 +32,24 @@ def login():
 
 @auth_bp.route('/logout', methods=['GET', 'POST'])
 def logout():
-    
     #Create logout route implementation here:
-    session.pop('user_id', None)
+    session.pop('username', None)
     flash('You have been logged out.', 'info')
     return redirect(url_for('index'))
 
 @auth_bp.route('/register', methods=['GET', 'POST'])
-def route_register_page():
+def register():
     print("Rekisteri sivulle ohjaus")
     return render_template('register.html')
 
-@auth_bp.route('/register', methods=['GET', 'POST'])
-def register():
-    
+@auth_bp.route('/registerHandle', methods=['GET', 'POST'])
+def registerHandle():
+    print("login toimii")
     #Create register route implementation here:
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        password_verify = request.form['password_verify']
+        password_verify = request.form['password-verify']
         
         user = User(dynamodb.Table('UserData'))
         
